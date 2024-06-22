0386bf08f1bf73fbc898f728a55301c139be1235
Sebastian <sebasdc2003@gmail.com>
2024-06-22 15:48:04 -0500

    segundo commit

--- app.js
+++ app.js
@@ -1,10 +1,23 @@
 let numeroSecreto = 6;
-let numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+let numeroUsuario = 0;
+let intentos = 1;
+let palabraVeces = "vez";
+while (numeroUsuario != numeroSecreto) {
+  numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
 
-console.log(numeroUsuario);
+  console.log(numeroUsuario);
 
-if (numeroUsuario == numeroSecreto) {
-  alert(`Acertaste, el número es: ${numeroUsuario}`);
-} else {
-  alert("No acertaste el número");
+  if (numeroUsuario == numeroSecreto) {
+    alert(
+      `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`
+    );
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      alert("El número secreto es mayor");
+    }
+    intentos = intentos + 1;
+    palabraVeces = "veces";
+  }
 }
