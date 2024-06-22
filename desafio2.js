7464c53b6b3a858284011fc393f27e3213abfee2
Sebastian <sebasdc2003@gmail.com>
2024-06-22 14:39:59 -0500

    first commit

---
+++ desafio2.js
@@ -0,0 +1,36 @@
+//1.Pregunta al usuario qué día de la semana es. Si la respuesta es "Sábado" o "Domingo", muestra "¡Buen fin de semana!". De lo contrario, muestra "¡Buena semana!".
+
+/*let dayWeek = prompt("Día de la semana");
+if (dayWeek == "Sábado" || dayWeek == "Domingo") {
+  alert("Buen fin de semana");
+} else {
+  alert("Buena semana");
+}*/
+
+//2.Verifica si un número ingresado por el usuario es positivo o negativo. Muestra una alerta informativa.
+
+/*let numero = prompt("Ingresa un número");
+if (numero >= 0) {
+  alert(`El numero es positivo: ${numero} `);
+} else {
+  alert("El número es negativo");
+}*/
+
+//3.Crea un sistema de puntuación para un juego. Si la puntuación es mayor o igual a 100, muestra "¡Felicidades, has ganado!". En caso contrario, muestra "Intentalo nuevamente para ganar.".
+
+/*let puntuacion = 200;
+if (puntuacion >= 100) {
+  alert("Felicidades, has ganado");
+} else {
+  alert("Intentalo nuevamente para ganar");
+}*/
+
+//4.Crea un mensaje que informe al usuario sobre el saldo de su cuenta, utilizando un template string para incluir el valor del saldo.
+
+/*let saldo = 2100;
+alert(`Su saldo es de: ${saldo}`);*/
+
+//5.Pide al usuario que ingrese su nombre mediante un prompt. Luego, muestra una alerta de bienvenida usando ese nombre.
+
+/*let nombre = prompt("Ingrese su nombre");
+alert(`Bienviendo al sistema ${nombre}`);*/
