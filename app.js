05bd54c57f465f92bc8866efcb0660b6bd737b20
Sebastian <sebasdc2003@gmail.com>
2024-06-23 19:43:07 -0500

    juego numero secreto terminado

--- app.js
+++ app.js
@@ -1,26 +1,34 @@
-let numeroSecreto = 6;
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random() * numeroMaximoPosible) + 1;
 let numeroUsuario = 0;
 let intentos = 1;
-let palabraVeces = "vez";
+let maximosIntentos = 6;
+
 while (numeroUsuario != numeroSecreto) {
-  numeroUsuario = prompt("Me indicas un número entre 1 y 10 por favor:");
+  numeroUsuario = parseInt(
+    prompt(`Me indicas un número entre 1 y ${numeroMaximoPosible}  por favor:`)
+  );
 
-  console.log(numeroUsuario);
-
+  console.log(typeof numeroUsuario);
   if (numeroUsuario == numeroSecreto) {
+    //Acertamos, fue verdadera la condición
     alert(
-      `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${palabraVeces}`
+      `Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${
+        intentos == 1 ? "vez" : "veces"
+      }`
     );
   } else {
     if (numeroUsuario > numeroSecreto) {
-      alert("El numero secreto es menor");
+      alert("El número secreto es menor");
     } else {
       alert("El número secreto es mayor");
     }
-    intentos = intentos + 1;
-    palabraVeces = "veces";
-    if (intentos > 3) {
-      alert("Llegaste al máximo de intentos");
+    intentos++;
+
+    //palabraVeces = 'veces';
+    if (intentos > maximosIntentos) {
+      alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
       break;
     }
   }
